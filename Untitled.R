cb1048f0e7289547657cce0f2b702f8f8fc02401
DEaJspGRi <87737729+Dean-Granic@users.noreply.github.com>
2022-06-15 13:51:47 +1000

    Add files via upload

---
+++ Untitled.R
@@ -0,0 +1,9 @@
+print('new r script')
+print('some other changes to the script')
+func = function(x){
+  print(x = x + 1)
+}
+func(1)
+print('us us us and them them them them')
+
+print("I have 2 doges named Jessie and Luna")
